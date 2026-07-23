2ec928a008b1ae44c62f43ce9f69662707b62ffb
Prahlad Agarwal <prahladagarwal575@gmail.com>
2026-07-23 12:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 125.6 MB (10.20%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if(n <= 2) return n;
+
+        int bits = 32 - Integer.numberOfLeadingZeros(n);
+        return 1 << bits;
+    }
+}
